b29db4ff79e1f07ef32ea85f2bd7b311459e00f8
kirankgarimella <40698459+kirankgarimella@users.noreply.github.com>
2020-01-22 18:28:55 -0500

    Version 2 of HelloWorld

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is another line! Will this sync without build?");
         }
     }
 }
